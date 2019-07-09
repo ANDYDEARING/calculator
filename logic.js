40e81a6f32294316e3caab0becd671e89501d1b7
Andy Dearing <andy.dearing@gmail.com>
2019-07-09 17:17:02 -0400

    working display area with buttons

--- logic.js
+++ logic.js
@@ -1,8 +1,21 @@
-
+function evaluateExpression(expression){
+    return 55
+}
 
 let buttonList = document.querySelectorAll(".calc-button")
 for (let button of buttonList){
     button.addEventListener('click', function(){
-        document.querySelector(".display").value += button.value
+        if (button.value==="equals"){
+            expression = document.querySelector(".display").value
+            document.querySelector(".display").value = evaluateExpression(expression)
+        } else if (button.value==="clear"){
+            document.querySelector(".display").value = "0"
+        } else {
+            if (document.querySelector(".display").value === "0"){
+                document.querySelector(".display").value = button.value
+            } else {
+                document.querySelector(".display").value += button.value
+            }
+        }
     })
 }
