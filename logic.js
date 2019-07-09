9a639d6bc2b799f550f9285a257d4442ae094a86
Andy Dearing <andy.dearing@gmail.com>
2019-07-09 17:48:21 -0400

    basic functions and some stretch works

--- logic.js
+++ logic.js
@@ -7,11 +7,12 @@ for (let button of buttonList){
     button.addEventListener('click', function(){
         if (button.value==="equals"){
             expression = document.querySelector(".display").value
-            document.querySelector(".display").value = evaluateExpression(expression)
+            document.querySelector(".display").value = `= ${evaluateExpression(expression)}`
         } else if (button.value==="clear"){
             document.querySelector(".display").value = "0"
         } else {
-            if (document.querySelector(".display").value === "0"){
+            if ( (document.querySelector(".display").value === "0") || 
+                    (document.querySelector(".display").value[0] === "=" ) ) {
                 document.querySelector(".display").value = button.value
             } else {
                 document.querySelector(".display").value += button.value
